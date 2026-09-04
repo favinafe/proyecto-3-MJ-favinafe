cacbddabc4821df5469e0466d899f001608b317d
favinafe <favinafe@gmail.com>
2026-09-04 15:46:54 -0300

    feat: Se creo el script sql principal.sql el cual ejecuta los demas scripts

---
+++ elementosComunes/baseDeDatos/principal.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS SIGSM;
+USE SIGSM;
+
+SOURCE moduloDocs/tablasDocs.sql;
+SOURCE moduloDocs/insertDocs.sql;
+
+SOURCE moduloAmb/tablasAmb.sql;
+SOURCE moduloAmb/insertAmb.sql;
+
+SOURCE moduloUsr/tablasUsr.sql;
+SOURCE moduloUsr/insertUsr.sql;
+
